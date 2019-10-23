ceeb22806cb0fb328034b43056ce5f80f06d7cda
ZanePartain <zane.partain@wsu.edu>
2019-10-22 20:04:42 -0700

    Reimplementing my Agent in a much more clean way. Agent never dies to wumpus. TODO: implement arrow path on KB, and implement gold road, and A* search for quickest path back to start after GRABing gold

--- PyAgent.py
+++ PyAgent.py
@@ -12,22 +12,20 @@ class PyAgent:
 
     def __init__(self):
         self.loc = [1,1]       # Agent start location
+        self.path = [(1,1)]    # path of agent
         self._arrow = True     # Agent starts with arrow
+        self.arrow_path = None # Arrow path ; orientation, and respective coord
+           
         self._gold = False     # Agent starts w/o gold
         self.visited = [(1,1)] # all locations the agent has visited
         self.stenches = []     # all of the stench locations visited
         self.wumpus = []       # wumpus locations
-        self.gold_road = []    # path to previous gold 
-        self.golden_ticket = False   # let agent know they are on previous known gold path
         self._wumpus = True    # wumpus alive ? true : false
-        self._sorted = True    # whether or not the visited list is sorted
         self.orientation = Orientation.RIGHT # Agent starts right
-        self.prev_loc = None
         
         # flags for when bump is true
         self.bump = False 
-        self.x = False 
-        self.y = False
+
 
 
     # return a random action turn 
@@ -49,48 +47,137 @@ class PyAgent:
 
     # this is for ensuring the locations stays in bounds
     # decrement the most recently updated direction by 1
-    def dec_location(self):
-        if  self.x == True:
-            self.loc[0] -= 1
-            self.x = False
-        if self.y == True:
-            self.loc[1] -= 1
-            self.y = False
+    def dec(self):
+        # print('(TO PREV LOC)')
+        # print('(USING PREV LOC! )', self.path[-1])
+        self.loc[0], self.loc[1] = self.path.pop()
         self.bump = False
+        # print('UPDATED_LOC ', self.loc)
+
                 
 
     # update the current location of agent
     def update_location (self, real=True):
         x,y = self.loc
         orientation = self.orientation
+        self.path.append((self.loc[0], self.loc[1])) # before updating add to path
+        # print('PREV LOC:', self.path[-1])
     
         # only when the bump flag is false increment the x & y
-        if orientation == Orientation.RIGHT:
-            if self.bump is False:
-                x += 1
-                self.x = True
-        elif orientation == Orientation.LEFT and x > 1:
+        if orientation == Orientation.LEFT and x > 1:
             x -= 1
-        elif orientation == Orientation.UP:
-           if self.bump is False:
-                y += 1
-                self.y = True
         elif orientation == Orientation.DOWN and y > 1:
             y -= 1
+        if orientation == Orientation.RIGHT:
+            x += 1
+        elif orientation == Orientation.UP:
+            y += 1
 
         self.loc[0] = x
         self.loc[1] = y
 
-
-        #only update visited if its a real move and not used for agent logic 
-        if real is True:
-            if (self.loc[0], self.loc[1]) not in self.visited:
-                self.visited.append((self.loc[0], self.loc[1]))
+        # print('UPDATED_LOC ', self.loc)
 
 
-    # find all potential wumpus locations given 
-    # current stench locations on the board
-    def find_wumpus(self):
+    def KB_AGENT(self, percept_str=str):
+        percepts = percept_str.strip().split(',')
+        rand_move = random.randint(1,51)
+        
+        percept_dict = {}
+        for p in percepts:
+            x,y = p.split('=')
+            percept_dict[x] = y
+        
+        # agent has gold and is at start loc
+        if self._gold and self.loc == [1,1]:
+            return Action.CLIMB
+            #implement logic for agent to find his way back home safely/ shortest path
+            # A* search
+
+        if percept_dict['Stench'] == 'True':
+            # shoot arrow, and keep track of all cells that the arrow passes
+            if self._arrow:
+                self._arrow = False
+
+                # TODO incorporate this into a function so you can ential wumpus locations in KB_entail_wumpus()
+                # keep track of y coordinate for RIGHT or LEFT
+                if self.orientation == Orientation.RIGHT:
+                    # every cell right of the current position can't have a wumpus
+                    print('Arrow path = every cell RIGHT of {} has no Wumpus'.format(self.loc))
+                   
+                elif self.orientation == Orientation.LEFT:
+                    # every cell left of the current position can't have a wumpus
+                    print('Arrow path = every cell LEFT of {} has no Wumpus'.format(self.loc))
+                
+                # keep track of x coordinate for UP or DOWN
+                elif self.orientation == Orientation.UP:
+                    # every cell above of the current position can't have a wumpus
+                    print('Arrow path = every cell ABOVE {} has no Wumpus'.format(self.loc))                  
+                    
+                elif self.orientation == Orientation.DOWN:
+                    # every cell below of the current position can't have a wumpus
+                    print('Arrow path = every cell BELOW {} has no Wumpus'.format(self.loc))                   
+
+                # save the orientation and current location
+                self.arrow_path = (self.orientation, (self.loc[0],self.loc[1]))
+                return Action.SHOOT
+                
+            # add current loc to KB stenches
+            if (self.loc[0],self.loc[1]) not in self.stenches:
+                self.stenches.append((self.loc[0],self.loc[1]))
+
+            # entail wumpus locations from KB
+            if self._wumpus is True:
+                self.KB_entail_wumpus()
+
+        if percept_dict['Breeze'] == 'True':
+            pass
+        if percept_dict['Glitter'] == 'True':
+            self._gold = True
+            return Action.GRAB
+        if percept_dict['Bump'] == 'True':
+            self.bump = True
+            action = self.random_turn()
+            self.update_orientation(action)
+            self.dec()
+            return action
+        if percept_dict['Scream'] == 'True':
+            # update all potential wumpus areas to safe
+            self._wumpus = False
+            self.wumpus = []####### ####### #######
+        
+
+
+        # randomly go forward or turn
+        if (rand_move % 2 == 0):
+            # add current location to visited if not already
+            if (self.loc[0],self.loc[1]) not in self.visited:
+                self.visited.append((self.loc[0],self.loc[1]))
+            
+            self.update_location()  # update current location
+
+            # if the updated location is not safe then dec location
+            # and dont go forward ; perform random turn
+            if (self.loc[0], self.loc[1]) in self.wumpus:
+                self.dec()
+                # random turn
+                action = self.random_turn()
+                self.update_orientation(action)
+                return action
+
+            print('VISITED: ', self.visited)
+            return Action.GOFORWARD
+        else: 
+            # random turn
+            action = self.random_turn()
+            self.update_orientation(action)
+            return action
+
+
+
+    # find all potential wumpus locations given KB
+    # of current stench locations on the board
+    def stench_to_wump(self):
         if not self.bump:
             if len(self.stenches) == 1:
                 # this will be for when you only have 1 stench loc calculate 
@@ -145,30 +232,6 @@ class PyAgent:
                     return [(self.stenches[x][0], self.stenches[y][1])]
 
 
-    #returns an array of all possible safe moves from given location
-    def possible_moves(self):
-        # unvisited flag will determine whether or not you want
-        # to get all possible moves that are safe
-        moves = []
-
-        # ensure the UP and RIGHT directions are safe 
-        if (self.loc[0] + 1, self.loc[1]) not in self.wumpus:
-            moves.append((self.loc[0] + 1, self.loc[1])) # x + 1
-        if (self.loc[0], self.loc[1] + 1) not in self.wumpus:
-            moves.append((self.loc[0], self.loc[1] + 1)) # y + 1
-
-        # ensure LEFT and DOWN directions are in bounds and safe
-        if self.loc[0] > 1:
-            if (self.loc[0]-1, self.loc[1]) not in self.wumpus:
-                moves.append((self.loc[0] - 1, self.loc[1])) # x - 1
-        if self.loc[1] > 1:
-            if (self.loc[0], self.loc[1] - 1) not in self.wumpus:
-                moves.append((self.loc[0], self.loc[1]-1)) # y - 1
-        
-        return moves
-
-
-
     # calc all the potential stench locations for all potential wumpus locations.
     # if the potential stench cell is not in the stenches[] but is in visited[]
     # we can say that there is no wumpus in the potential wumpus location.
@@ -187,23 +250,34 @@ class PyAgent:
 
     #calculate all potential wumpus locations.
     #keeping in mind all visited and stench locations.
-    def calc_wumpus_loc(self):
+    def KB_entail_wumpus(self):
+        print('STENCHES: ', self.stenches)
         if not self.bump:
-            if (self.loc[0],self.loc[1]) not in self.stenches:
-                self.stenches.append((self.loc[0],self.loc[1]))
-
             # if there is only one wumpus location then we know where the wumpus is
-            if len(self.wumpus) is not 1:
-                self.wumpus = self.find_wumpus() #list of potential wumpus locations
+            if len(self.wumpus) != 1:
+               
+                # list[()] of potential wumpus locations by KB stenches
+                self.wumpus = self.stench_to_wump()
+                print('WUMPUS LOC: ', self.wumpus)
                 
-                # check if a potential wumpus location has already been 
-                # visited. If it has, then there is no wumpus there.
-                for i in range(len(self.visited)):
-                    if self.visited[i] in self.wumpus:
-                        self.wumpus.remove(self.visited[i])
-                
+                # check if a potential wumpus location has already been visited.
+                # If so, it is a safe location
+                for space in self.wumpus:
+                    if space in self.visited:
+                        print(' REMOVE {} from potential wumpus locations | (VISITED) => ~WUMPUS'.format(space))
+                        self.wumpus.remove(space)
+
+                # TODO look at KB_AGENT() Stench=True for inspo
+                # check if the potential wumpus locations are in KB arrow_path:
+                if self.arrow_path is not None:
+                    # compare the potential wumpus locations to the arrow path
+                    # if they intersect, then delete the potential wumpus location
+                    pass 
+
                 # calc all stench locations from potential wumpus locations
                 wump_stenches = self.wump_to_stench() 
+                for loc, stench in wump_stenches.items():
+                    print(" @ WUMPLOC {}: stenches = {}".format(loc,stench))
 
                 # if potential stench location is in visited[] but not in stenches[],
                 # there can't be a wumpus at that potential wumpus location
@@ -211,79 +285,18 @@ class PyAgent:
                     # print(loc, stench_list)
                     for item in stench_list:
                         if item in self.visited and item not in self.stenches:
-                            # print(item)
                             if loc in self.wumpus:
+                                print(' REMOVE {} from potential wumpus locations | (VISITED ^ ~STENCH) => ~WUMPUS'.format(loc))
                                 self.wumpus.remove(loc)
 
-                print("WUMPUS POTENTIAL LOC UPDATED: ", self.wumpus)
-                print("STENCHES UPDATED: ", agent.stenches)
+                print("WUMPUS LOC UPDATED: ", self.wumpus)
+            else:
+                print('WUMPUS FOUND AT {}'.format(self.wumpus))
 
                    
     # return True or False whether or not the go forward action is rational
     def logical_move(self):
-        move = ()
-        x, y = self.loc
-        safe = False 
-        self.update_location(False) # set the agent forward theoretically
-        print("WUMPUS POTENTIAL LOC UPDATE 2.0: ", self.wumpus)
-
-        if self._gold is False and len(self.gold_road) > 0:
-            gold_move = self.gold_road.pop()
-            if gold_move not in self.wumpus:
-                print("HIHIHIHIHH")
-                safe = True
-                move = gold_move
-                self.golden_ticket = True
-            elif (self.loc[0], self.loc[1]) not in self.wumpus:
-                safe = True 
-                move = (self.loc[0], self.loc[1])
-        else:
-            if (self.loc[0], self.loc[1]) not in self.wumpus:
-                safe = True 
-                move = (self.loc[0], self.loc[1])
-
-        # decrement the theoretical location back to actual location
-        self.dec_location()
-        self.loc[0] = x
-        self.loc[1] = y
-
-        return safe, move
-
-
-    # set a path for the agent to return home on the quickest path it has traveled
-    def go_home(self):
-        safe_moves = self.possible_moves() 
-        print("SAFE MOVES: ", safe_moves)
-
-        self.visited.sort() # sort all visited space ascending 
-
-        # go through all locations in ascending order (1,1) (1,2) ...
-        for loc in self.visited:
-                if loc in safe_moves and loc < (self.loc[0],self.loc[1]):
-                    # if the loc in the safe moves then you 
-                    # know the next best move
-                    print("NEXT BEST MOVE: ", loc)
-                    return loc
-
-    # push the cells on the path back from the gold to [1,1] in a stack.
-    # the stack will be used to for the agent to move to first (if safe)
-    def money_grabber(self, cell=None):
-        if cell is None:
-            self.gold_road.append((self.loc[0], self.loc[1]))
-        elif cell not in self.gold_road:
-            print('MONEY GRABBER: ', self.gold_road)
-            self.gold_road.append(cell)
-
-    
-    # return True if the next_move param is closer to [1,1] than the best move
-    def is_closer(self, next_move, best_move):
-        # this does not account for closest to [1,1] yet
-        # needs work... ... ...
-        if next_move < best_move:
-            print("NEXT MOVE < BEST MOVE")
-            return True
-
-        return False
+       pass
 
 
 agent = PyAgent() # init agent
@@ -301,58 +314,15 @@ count = 0
 
 def PyAgent_Process (stench,breeze,glitter,bump,scream):
     global agent
-    print("GOLD ROAD:", agent.gold_road)
     perceptStr = ""
     _flag = random.randint(0,10001)
     print('location: ', agent.loc)
     print('orientation: ', agent.orientation)
     print('arrow: ', agent._arrow)
     print('gold: ', agent._gold)
-    print('VISITED: ', agent.visited)
-    print("STENCHES: ", agent.stenches)
-    print("WUMPUS POTENTIAL LOC: ", agent.wumpus)
-    
-    # if agent has gold; GO HOME or CLIMB
-    if agent._gold and bump != 1:
-        if agent.loc == [1,1]:
-            return Action.CLIMB
-
-        best_move = agent.go_home() # return on quickest path back to start one cell at a time
-        safe, next_move = agent.logical_move()  # check if going forward is safe
-
-        print('NEXT MOVE: ', next_move)
-        print('BEST move:', best_move)
-        # the best theoretical move to get to [1,1] 
-        # from current location is also the next potential move
-        if next_move == best_move:
-            agent.update_location()
-            agent.money_grabber(best_move)
-            return Action.GOFORWARD
-
-        # if you were to move forward and the area is safe (visited or unvisited)
-        # and it gets you closer to [1,1] than your best move. Then GOFORWARD!!
-        elif safe is True and best_move is None:
-            agent.update_location()
-            agent.money_grabber(next_move)
-            return Action.GOFORWARD
-
-        # your best theoretical move is not your potential next move. And your
-        # potential next move is either not safe, or will take you in the wrong direction
-        else:
-            action = Action.TURNLEFT
-            agent.update_orientation(action)
-            return action
-
-        
-
 
     if (stench == 1):
         perceptStr += "Stench=True,"
-        agent.calc_wumpus_loc()
-        # don't kill wumpus for this sim
-        #if agent._arrow:
-        #   agent._arrow = False
-        #   return Action.SHOOT
     else:
         perceptStr += "Stench=False,"
     if (breeze == 1):
@@ -361,50 +331,18 @@ def PyAgent_Process (stench,breeze,glitter,bump,scream):
         perceptStr += "Breeze=False,"
     if (glitter == 1):
         perceptStr += "Glitter=True,"
-        agent._gold = True
-       
-        agent.money_grabber()
-        return Action.GRAB
     else:
         perceptStr += "Glitter=False,"
     if (bump == 1):
         perceptStr += "Bump=True,"
-        agent.bump = True
-        action = agent.random_turn()
-        agent.update_orientation(action)
-        agent.dec_location()
-        return action
     else:
         perceptStr += "Bump=False,"
     if (scream == 1):
         perceptStr += "Scream=True"
     else:
         perceptStr += "Scream=False"
-    print("PyAgent_Process: " + perceptStr)
-    
 
-    # update location then move
-    # randomly turn or go forward (if can) otherwise turn 
-    safe, move = agent.logical_move()
-
-    if safe is True:
-        if agent.golden_ticket is True:
-            agent.golden_ticket = False
-            print('TO MOVE GOLDEN TICKET: ', move)
-            agent.update_location()
-            return Action.GOFORWARD
-        if _flag % 2 == 0:
-            print('TO MOVE: ', move)
-            agent.update_location()
-            return Action.GOFORWARD
-        
-    # randomly turn
-    if agent.loc == [1,1]:
-        agent.update_location()
-        return Action.GOFORWARD
-    action = agent.random_turn()
-    agent.update_orientation(action)
-    return action
+    return agent.KB_AGENT(perceptStr)
     
 
 
